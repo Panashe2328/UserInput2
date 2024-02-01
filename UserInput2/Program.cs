a2072ff7044923dc0453a7b960bc94e58ca49cee
ULTRACASH <ULTRACASH@DESKTOP-BFE61S3>
2024-02-01 12:21:42 -0930

    Add project files.

---
+++ UserInput2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1, num2, num3;
+
+Console.WriteLine("Enter the first number ");
+ num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second  number ");
+num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the third  number ");
+num3 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(num1*num2*num3);
